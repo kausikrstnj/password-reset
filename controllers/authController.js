e74c07b8d5c11084fd6599b05038563876a1c53e
kausikrstnj <kausikrstnj@gmail.com>
2024-03-05 22:28:26 +0530

    login ejs

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,26 +1,74 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
+const crypto = require('crypto');
+const nodemailer = require('nodemailer');
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-// Home 
-exports.getHome = async (req, res) => {
-    try {
-        res.render("home");
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Internal Server Error");
+
+
+
+// Generate a random string
+function generateRandomString(length) {
+    let result = '';
+    const characters = '0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
-};
+    return result;
+}
+
+// Function to send OTP link via email
+async function sendOtpEmail(email, otp) {
+    try {
+        const msg = {
+            to: email,
+            from: 'your_email@example.com', // Use the email you verified with SendGrid
+            subject: 'OTP for Login',
+            text: `Your OTP for login is ${otp}.`,
+            html: `<p>Your OTP for login is <strong>${otp}</strong>.</p>`
+        };
+        await sgMail.send(msg);
+        console.log('Email sent successfully');
+    } catch (error) {
+        console.error('Error occurred while sending email:', error);
+    }
+}
+
+
 
 //otp page
 exports.otp = async (req, res) => {
     try {
-        res.render("otp");
+        //Dummy
+        const otp = generateRandomString(6); // You can adjust the length as needed
+        console.log('otp :', otp)
+        const userEmail = 'kausikrstnj@gmail.com';
+
+        // Send the OTP via email
+        await sendOtpEmail(userEmail, otp);
+        //
+        res.status(200).json({ success: true, message: "Please check your email for the One Time Password." });
+
+        // res.render("otp");
     } catch (error) {
         console.error(error);
         res.status(500).send("Internal Server Error");
     }
 };
+
+// Home 
+exports.getHome = async (req, res) => {
+    try {
+        res.render("login");
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
+};
+
 
 //signUp page
 exports.signUp = async (req, res) => {
